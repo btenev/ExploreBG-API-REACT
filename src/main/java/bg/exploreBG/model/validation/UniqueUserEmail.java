793767b3425e0e563adc1303dc33c006901756db
btenev <boyan.tenev@gmail.com>
2024-06-13 12:27:04 +0300

    Create custom validation annnotations for unique email, unique username, field match

---
+++ src/main/java/bg/exploreBG/model/validation/UniqueUserEmail.java
@@ -0,0 +1,20 @@
+package bg.exploreBG.model.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = UniqueUserEmailValidator.class)
+public @interface UniqueUserEmail {
+    String message() default "Invalid Email";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
---
+++ src/main/java/bg/exploreBG/model/validation/UniqueUsernameValidator.java
@@ -0,0 +1,20 @@
+package bg.exploreBG.model.validation;
+
+import bg.exploreBG.repository.UserRepository;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class UniqueUsernameValidator implements ConstraintValidator<UniqueUsername, String> {
+    private final UserRepository userRepository;
+
+    public UniqueUsernameValidator(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        return this.userRepository
+                .findByEmail(value)
+                .isEmpty();
+    }
+}
