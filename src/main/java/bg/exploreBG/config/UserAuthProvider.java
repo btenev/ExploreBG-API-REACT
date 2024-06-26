4f76586403abff5ff052a2b94ee0d0ace3a4f414
btenev <boyan.tenev@gmail.com>
2024-06-26 13:29:22 +0300

    Chage token validity to 8,5hours

--- src/main/java/bg/exploreBG/config/UserAuthProvider.java
+++ src/main/java/bg/exploreBG/config/UserAuthProvider.java
@@ -33,7 +33,7 @@ public class UserAuthProvider {
 
     public String createToken(String login) {
         Date now = new Date();
-        Date validity = new Date(now.getTime() + 3_600_000);
+        Date validity = new Date(now.getTime() + 30_600_000);
 
         return JWT.create()
                 .withIssuer(login)
