07193c98c4bac0f7f2cb38b3a172c8df144503d8
btenev <boyan.tenev@gmail.com>
2024-09-17 17:27:44 +0300

    Add creationDate field to GpxEntity to track when GPX records are created

--- src/main/java/bg/exploreBG/model/entity/GpxEntity.java
+++ src/main/java/bg/exploreBG/model/entity/GpxEntity.java
@@ -2,6 +2,8 @@ package bg.exploreBG.model.entity;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Table(name = "gpxs")
 public class GpxEntity {
@@ -10,44 +12,40 @@ public class GpxEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
     private String cloudId;
-
     private String folder;
-
     private String gpxUrl;
-
+    private LocalDateTime creationDate;
     public GpxEntity() {
     }
-
     public Long getId() {
         return Id;
     }
-
     public void setId(Long id) {
         Id = id;
     }
-
     public String getCloudId() {
         return cloudId;
     }
-
     public void setCloudId(String cloudId) {
         this.cloudId = cloudId;
     }
-
     public String getGpxUrl() {
         return gpxUrl;
     }
-
     public void setGpxUrl(String gpxUrl) {
         this.gpxUrl = gpxUrl;
     }
-
     public String getFolder() {
         return folder;
     }
-
     public void setFolder(String folder) {
         this.folder = folder;
+    }
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
     }
 
     @Override
@@ -57,6 +55,7 @@ public class GpxEntity {
                 ", cloudId='" + cloudId + '\'' +
                 ", folder='" + folder + '\'' +
                 ", gpxUrl='" + gpxUrl + '\'' +
+                ", creationDate=" + creationDate +
                 '}';
     }
 }
