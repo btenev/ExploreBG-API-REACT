60d8c72e4cdcfd3b3aae37e7be2869ac48cefd5b
btenev <boyan.tenev@gmail.com>
2025-04-04 16:16:18 +0300

    Add CookieProperties configuration for HTTP-only cookies.

---
+++ src/main/java/bg/exploreBG/config/CookieProperties.java
@@ -0,0 +1,18 @@
+package bg.exploreBG.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "app.cookie")
+public class CookieProperties {
+    private boolean secure;
+
+    public boolean isSecure() {
+        return secure;
+    }
+
+    public void setSecure(boolean secure) {
+        this.secure = secure;
+    }
+}
