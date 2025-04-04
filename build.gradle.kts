abdba73c1c5289de4ea6e868156f02467a4e42b5
btenev <boyan.tenev@gmail.com>
2025-04-04 15:52:48 +0300

    Add Redis dependency and initial configuration.

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-data-redis")
+	implementation("org.apache.commons:commons-pool2")
 	// https://mvnrepository.com/artifact/com.auth0/java-jwt
 	implementation("com.auth0:java-jwt:4.4.0")
 	// https://mvnrepository.com/artifact/com.cloudinary/cloudinary-http44
